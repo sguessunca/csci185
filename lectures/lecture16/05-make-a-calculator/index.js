b5eb0aa7d6f6419f16b941c86c8e17746528baa2
sguessunca <sguess@unca.edu>
2023-03-20 23:56:09 -0400

    com

--- lectures/lecture16/05-make-a-calculator/index.js
+++ lectures/lecture16/05-make-a-calculator/index.js
@@ -2,6 +2,15 @@ function addNumbers() {
     // Your job: 
     // 1. Access the first number that the user typed in and store it
     //    as a variable.
+    let firstNumber=document.querySelector('#num1').value;
+    firstNumber=Number(firstNumber);
+    let secondNumber = document.querySelector('#num2').value;
+    secondNumber=Number(secondNumber)
+    let add = firstNumber + secondNumber; 
+
+    document.querySelector('#answer').innerHTML = add;
+
+    
     // 2. Access the first number that the user typed in and store it
     //    as a variable.
     // 3. Convert the values stored inside of the two variables to a 
@@ -12,13 +21,35 @@ function addNumbers() {
 
 function subtractNumbers() {
     // Same as above but subtract!
+    let firstNumber=document.querySelector('#num1').value;
+    firstNumber=Number(firstNumber);
+    let secondNumber = document.querySelector('#num2').value;
+    secondNumber=Number(secondNumber)
+    let subtract = firstNumber - secondNumber; 
+
+    document.querySelector('#answer').innerHTML = subtract;
+
 }
 
 function multiplyNumbers() {
     // Same as above but multiply!
+    let firstNumber=document.querySelector('#num1').value;
+    firstNumber=Number(firstNumber);
+    let secondNumber = document.querySelector('#num2').value;
+    secondNumber=Number(secondNumber)
+    let multiply = firstNumber * secondNumber; 
+
+    document.querySelector('#answer').innerHTML = multiply;
 }
 
 function divideNumbers() {
     // Same as above but divide!
-}
+    let firstNumber=document.querySelector('#num1').value;
+    firstNumber=Number(firstNumber);
+    let secondNumber = document.querySelector('#num2').value;
+    secondNumber=Number(secondNumber)
+    let divide = firstNumber / secondNumber; 
+
+    document.querySelector('#answer').innerHTML = divide;
+} 
 
