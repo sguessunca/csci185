6bcd5193384cdb477df49badef5f6b1e34926552
sguessunca <sguess@unca.edu>
2023-03-24 23:56:51 -0400

    c

--- lectures/lecture17/04-drawings/sketch.js
+++ lectures/lecture17/04-drawings/sketch.js
@@ -4,10 +4,40 @@ function setup() {
     createCanvas(canvasWidth, canvasHeight);
     background("pink");
 
+    drawCreature();
+
 
     // add your drawing here:
     // https://p5js.org/reference/#/p5/point
     // point(110, 20);
+
+    drawGrid(canvasWidth, canvasHeight)
+}
+
+
+    function drawCreature {
+    point(30, 20);
+    point(30, 31);
+    point(30, 42);
+    point(30, 53);
+    point(30, 64);
+    point(85, 20);
+    point(85, 31);
+    point(85, 42);
+    point(85, 53);
+    point(85, 64);
+    point(85, 75);
+    point(30, 75);
+    point(41, 20);
+    point(52, 20);
+    point(63, 20);
+    point(74, 20);
+    point(41, 75);
+    point(52, 75);
+    point(63, 75);
+    point(74, 75);
+    strokeWeight(11)
+    }
 
     // https://p5js.org/reference/#/p5/text
     // text("Here is some text", 200, 40);
@@ -28,5 +58,4 @@ function setup() {
 
 
     // for debugging:
-    drawGrid(canvasWidth, canvasHeight)
-}
+    
---
+++ tutorials/tutorial07/02-theme-switcher/index.js
@@ -0,0 +1,33 @@
+function defaultTheme() {
+    // your code here.
+    document.querySelector("body").className.reset
+    
+}
+
+
+function oceanTheme() {
+   // your code here.
+   document.querySelector("body").className="ocean";
+}
+   
+
+
+function desertTheme() {
+   // your code here.
+   document.querySelector("body").className="desert";
+}
+
+function highContrastTheme() {
+    // your code here.
+    document.querySelector("body").className="high-contrast";
+}
+
+/*
+    Hints: 
+    1. Attach the event handlers (functions) above to the click event
+       of each of the four buttons (#default, #ocean, #desert, 
+        and #high-contrast) in index.html.
+    2. Then, modify the  body of each function so that it
+       sets the className property of the body tag based on 
+       the button that was clicked.
+*/
